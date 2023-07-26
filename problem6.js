52166354fd824ad7ac4bbccf5d130f989a0c44cf
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-27 01:27:00 +0600

    finished

--- problem6.js
+++ problem6.js
@@ -22,8 +22,30 @@ const user = {
   },
 };
 
-console.log(user.email);
-console.log(user.address);
-console.log(user.address.city);
-console.log(user.address.geo.lat);
-console.log(user.company.name);
+// console.log(user.email);
+// console.log(user.address);
+// console.log(user.address.city);
+// console.log(user.address.geo.lat);
+// console.log(user.company.name);
+
+// const obj = { foo: 1 };
+// obj.bar = 2;
+// console.log(obj);
+
+// function min(nums) {
+//   return Math.min(nums);
+// }
+// console.log(min([1, 3, 2]));
+
+// const cube = (x) => x * x * x;
+// console.log(cube(2));
+
+// const [a, b] = [1, 2, 3, 4, 45, 5];
+// console.log(a + b);
+
+// const { x, y, z } = { x: 1, y1: 2, z: 3 };
+// console.log(y);
+
+const nums = [1, 2, 3, 4, 5];
+let output = nums.filter((n) => n % 2);
+console.log(output);
